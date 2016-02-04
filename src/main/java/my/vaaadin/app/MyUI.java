9ba16755272fe1afde095395cca1ca0b0fec9c59
Matti Tahvonen <matti@vaadin.com>
2016-02-04 14:51:31 +0200

    listing all Customers to Grid

--- src/main/java/my/vaaadin/app/MyUI.java
+++ src/main/java/my/vaaadin/app/MyUI.java
@@ -1,15 +1,16 @@
 package my.vaaadin.app;
 
+import java.util.List;
+
 import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
+import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
+import com.vaadin.ui.Grid;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -20,28 +21,31 @@ import com.vaadin.ui.VerticalLayout;
 @Widgetset("my.vaaadin.app.MyAppWidgetset")
 public class MyUI extends UI {
 
-    @Override
-    protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
+	private CustomerService service = CustomerService.getInstance();
+	private Grid grid = new Grid();
 
-        Button button = new Button("Click Me");
-        button.addClickListener( e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
-        });
-        
-        layout.addComponents(name, button);
-        layout.setMargin(true);
-        layout.setSpacing(true);
-        
-        setContent(layout);
-    }
+	@Override
+	protected void init(VaadinRequest vaadinRequest) {
+		final VerticalLayout layout = new VerticalLayout();
 
-    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
-    public static class MyUIServlet extends VaadinServlet {
-    }
+		grid.setColumns("firstName", "lastName", "email");
+		// add Grid to the layout
+		layout.addComponent(grid);
+
+		updateList();
+
+		layout.setMargin(true);
+		setContent(layout);
+	}
+
+	public void updateList() {
+		// fetch list of Customers from service and assign it to Grid
+		List<Customer> customers = service.findAll();
+		grid.setContainerDataSource(new BeanItemContainer<>(Customer.class, customers));
+	}
+
+	@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+	@VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+	public static class MyUIServlet extends VaadinServlet {
+	}
 }
