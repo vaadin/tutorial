f11a9b0c5e296e88bdcfe27befc2b17a2bed70f0
Alejandro Duarte <alejandro@vaadin.com>
2017-01-20 14:57:42 +0200

    Added filter

--- src/main/java/my/vaadin/app/MyUI.java
+++ src/main/java/my/vaadin/app/MyUI.java
@@ -6,11 +6,17 @@ import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.FontAwesome;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.shared.ui.ValueChangeMode;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.Grid;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -24,17 +30,30 @@ public class MyUI extends UI {
     
     private CustomerService service = CustomerService.getInstance();
     private Grid<Customer> grid = new Grid<>();
+    private TextField filterText = new TextField();
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         final VerticalLayout layout = new VerticalLayout();
 
+        filterText.setPlaceholder("filter by name...");
+        filterText.addValueChangeListener(e -> updateList());
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+
+        Button clearFilterTextBtn = new Button(FontAwesome.TIMES);
+        clearFilterTextBtn.setDescription("Clear the current filter");
+        clearFilterTextBtn.addClickListener(e -> filterText.clear());
+
+        CssLayout filtering = new CssLayout();
+        filtering.addComponents(filterText, clearFilterTextBtn);
+        filtering.setStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
+
         grid.addColumn(Customer::getFirstName).setCaption("First Name");
         grid.addColumn(Customer::getLastName).setCaption("Last Name");
         grid.addColumn(Customer::getEmail).setCaption("Email");
 
-        // add Grid to the layout
-        layout.addComponents(grid);
+        // add filtering and Grid components to the layout
+        layout.addComponents(filtering, grid);
 
         // fetch list of Customers from service and assign it to Grid
         updateList();
@@ -43,7 +62,7 @@ public class MyUI extends UI {
     }
 
     public void updateList() {
-        List<Customer> customers = service.findAll();
+        List<Customer> customers = service.findAll(filterText.getValue());
         grid.setItems(customers);
     }
 
